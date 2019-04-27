d08e77e999292db94b930705a7e6561786c38770
Gabriella Uwadiegwu <gabriellauwadiegwu@yahoomail.com>
2019-04-26 22:05:19 -0500

    more changes to tweet retrieval

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, request
 import re
 from tweets import Tweets
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/search', method=['GET', 'POST'])
+@app.route('/')
 def search():
 
     keyword = request.form['keyword']
     wordList = re.sub("[^\w]", " ", keyword).split()  # will strip punctuations later
-    tw = Tweets(wordList)
+    tw = Tweets()
 
-    tw.get_tweets()
+    for i in wordList:
+        tw.clean(i)
+
+    #return to the template
+
+    return render_template("index.html")
+
+@app.route('/')
+def results():
+
+    return render_template("results.html")
 
 if __name__ == '__main__':
     app.run()
--- tweets.py
+++ tweets.py
@@ -1,5 +1,8 @@
-from TwitterSearch import *
+import twitter
 import configparser
+from profanity_check import predict, predict_prob
+from functools import reduce
+import re
 
 config = configparser.ConfigParser()
 config.sections()
@@ -7,31 +10,54 @@ config.read('config.properties')
 
 class Tweets:
 
-    search_order = TwitterSearchOrder()
+    consumer_key = config["KEYS"]['consumer_key'].strip("\'")
+    consumer_secret = config["KEYS"]['consumer_secret'].strip("\'")
+    access_token = config["KEYS"]['access_token'].strip("\'")
+    access_token_secret = config["KEYS"]['access_token_secret'].strip("\'")
 
-    consumer_key = config["KEYS"]['consumer_key']
-    consumer_secret = config["KEYS"]['consumer_secret']
-    access_token = config["KEYS"]['access_token']
-    access_token_secret = config["KEYS"]['access_token_secret']
+    cleaned = []
+    details = {}
 
-    temp = []
+    def __init__(self):
 
-    def __init__(self, *keywords):
+        self.api = twitter.Api(consumer_key=self.consumer_key,
+                               consumer_secret=self.consumer_secret,
+                               access_token_key=self.access_token,
+                               access_token_secret=self.access_token_secret)
 
-        self.ts = TwitterSearch(
-            consumer_key = self.consumer_key,
-            consumer_secret = self.consumer_secret,
-            access_token = self.access_token,
-            access_token_secret = self.access_token_secret
-         )
+    def get_tweets(self, word):
 
-        self.search_order.set_keywords(keywords)
-        self.search_order.set_language('eng')
-        self.search_order.set_include_entities(False)
+        statuses = self.api.GetSearch(term=word,
+                                      count=100,
+                                      lang='en',
+                                      since="2018-01-01",
+                                      return_json=True)['statuses']
+        return [i['text'] for i in statuses]
 
-    def get_tweets(self):
+    def clean(self, word):
+        for i in self.get_tweets(word):
+            self.strip_out(i)
+        self.add_to_dict(word)
 
-        for tweet in self.ts.search_tweets_iterable(self.search_order):
-            self.temp.append(tweet['text'])
 
-        return self.temp #temporary just to see if I recieve tweets
+    def strip_out(self, text):
+
+        text = re.sub(r"http\S+", "", text)
+        text = re.sub(r'[^\w]', ' ', text).strip()
+        self.cleaned.append(text.strip())
+
+    def add_to_dict(self, word):
+        self.details[word] = self.cleaned
+        self.cleaned = []
+"""
+dummy tests
+t = Tweets()
+print(t.get_tweets())
+t.clean()
+
+p = predict_prob(t.cleaned)
+
+print(t.cleaned)
+print(reduce(lambda x, y: x*y, p.tolist()))
+print(p.tolist())
+"""
